c6b6700f6396fb1156d5778fbb9ea8766f48dc6c
Matheo <matheo.deknuydt@gmail.com>
2020-11-27 19:05:52 +0100

    render to the score of the round

--- js/Class/Player.js
+++ js/Class/Player.js
@@ -16,8 +16,8 @@ class Player {
         return 'scissors';
     }
 
-    stone() {
-        return 'stone';
+    rock() {
+        return 'rock';
     }
 
     play() {
@@ -27,7 +27,7 @@ class Player {
         } else if (number == 1) {
             return this.scissors();
         } else if (number == 2) {
-            return this.stone();
+            return this.rock();
         }
     }
 
@@ -38,35 +38,59 @@ let player2 = new Player('IA');
 let score1 = 0;
 let score2 = 0;
 
+const table = document.getElementById('rounds');
+const createLine = (score1, hand1, score2, hand2, win) => {
+    let tr = document.createElement('tr');
+    tr.classList.add('table-light');
+    let td0 = document.createElement('td');
+    td0.classList.add('text-right');
+    td0.textContent = hand1;
+    let td1 = document.createElement('td');
+    if (win == 0) {
+        td1.innerHTML = '<i class="fas fa-equals"></i> ' + score1 + ' | ' + score2 + ' <i class="text-secondary fas fa-equals"></i>';
+    } else if (win == 1) {
+        td1.innerHTML = '<i class="text-success fas fa-plus"></i> ' + score1 + ' | ' + score2 + ' <i class="text-danger fas fa-minus"></i>';
+    } else if (win == 2) {
+        td1.innerHTML ='<i class="text-danger fas fa-minus"></i> ' + score1 + ' | ' + score2 + ' <i class="text-success fas fa-plus"></i>';
+    }
+    td1.classList.add('text-center');
+    let td2 = document.createElement('td');
+    td2.textContent = hand2;
+    tr.appendChild(td0);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    table.appendChild(tr);
+}
+
 do {
     let hand1 = player1.play();
     let hand2 = player2.play();
 
     if (hand1 == hand2) {
-        console.log('null');
-    } else if (hand1 == 'paper' && hand2 == 'stone') {
+        createLine(score1, hand1, score2, hand2, 0);
+    } else if (hand1 == 'paper' && hand2 == 'rock') {
         score1++;
-        console.log('Player1 win this round');
+        createLine(score1, hand1, score2, hand2, 1);
     } else if (hand1 == 'paper' && hand2 == 'scissors') {
         score2++;
-        console.log('Player2 win this round');
-    } else if (hand1 == 'scissors' && hand2 == 'stone') {
+        createLine(score1, hand1, score2, hand2, 2);
+    } else if (hand1 == 'scissors' && hand2 == 'rock') {
         score2++;
-        console.log('Player2 win this round');
+        createLine(score1, hand1, score2, hand2, 2);
     } else if (hand1 == 'scissors' && hand2 == 'paper') {
         score1++;
-        console.log('Player1 win this round');
-    } else if (hand1 == 'stone' && hand2 == 'paper') {
+        createLine(score1, hand1, score2, hand2, 1);
+    } else if (hand1 == 'rock' && hand2 == 'paper') {
         score2++;
-        console.log('Player2 win this round');
-    } else if (hand1 == 'stone' && hand2 == 'scissors') {
+        createLine(score1, hand1, score2, hand2, 2);
+    } else if (hand1 == 'rock' && hand2 == 'scissors') {
         score1++;
-        console.log('Player1 win this round');
+        createLine(score1, hand1, score2, hand2, 1);
     }
 
     if (score1 == 3) {
-        console.log('Palyer 1 win this game !');
+        document.getElementById('winner').textContent = 'The player 1 win the game !';
     } else if (score2 == 3) {
-        console.log('Player 2 win this game !');
+        document.getElementById('winner').textContent = 'The player 1 win the game !';
     }
 } while (score1 != 3 && score2 != 3);
